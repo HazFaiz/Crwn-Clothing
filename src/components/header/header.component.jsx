5598ca9d4dacf8814b9a12a1912f68264869fb9a
Hazwan <haz.faizul@gmail.com>
2021-11-18 21:53:50 +0800

    added redux to user state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -13,9 +14,9 @@ const Header = ({currentUser}) => (
       <Logo className='logo' />
     </Link>
     <div className='options'>
-      <span>
+      {/* <span>
         {currentUser ? `Hello ${currentUser.displayName}` : null}
-      </span>
+      </span> */}
       <Link className='option' to='/shop'>
         SHOP
       </Link>
@@ -31,4 +32,8 @@ const Header = ({currentUser}) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
